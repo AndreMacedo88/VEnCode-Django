11dea9a8153c5b6a640cd107251d45e06763cfd6
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2020-05-20 14:15:59 +0100

    Make changes to allauth templates

--- vencode/urls.py
+++ vencode/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('table/', views.promoter_data_all, name='promoters')
+    # path('table/', views.promoter_data_all, name='promoters')
     # path('users/', include('django.contrib.auth.urls')),  # remove this when using allauth
 ]
